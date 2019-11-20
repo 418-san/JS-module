9b0576dc92f78fb222d936118b985cf8b32cb91e
418-san <fen1cs95@gmail.com>
2019-11-20 15:30:48 +0300

    HW_1 ( add js tasks, little fixes )

---
+++ Homework_1/js/1.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// 1_task
+let s_area = parseInt((10 * 100) + 'm2');
+let s_1gr = parseInt((15 * 25) + 'm2');
+let gr_amount = parseInt(s_area / s_1gr);
+console.log(gr_amount);
+let s_gr = gr_amount * s_1gr;
+console.log(s_gr);
+let s_free = s_area - s_gr;
+console.log(s_free + 'm2');
+
+// 2_task
+let sbig_ellipse = parseInt(15 + 'dm2');
+let ssmall_ellipse = parseInt((600 / 100) + 'dm2');
+let sring = sbig_ellipse - ssmall_ellipse;
+console.log(sring + 'dm2');
+
+// 3_task
+function popBox () 
+{
+    x = confirm(Math.min(9, 25, 13)); 
+
+    if (x == true )
+    {
+        document.write('Вы нажали ок');
+    }
+}
+
+// 4_ task
+let res;
+let m = 6;
+let n = 11;
+res = (m >= n)? "m ближе к 10": "n ближе к 10";
+console.log(res);
