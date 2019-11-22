5edc636a763a3c402017aab5c6ead19e38e1c898
418-san <fen1cs95@gmail.com>
2019-11-22 23:56:37 +0300

    4_ task little fixes

--- Homework_1/js/1.js
+++ Homework_1/js/1.js
@@ -1,8 +1,8 @@
 "use strict"
 
 // 1_task
-let s_area = parseInt((10 * 100) + 'm2');
-let s_1gr = parseInt((15 * 25) + 'm2');
+let s_area = parseInt( (10 * 100) + 'm2' );
+let s_1gr = parseInt( (15 * 25) + 'm2' );
 let gr_amount = parseInt(s_area / s_1gr);
 console.log(gr_amount);
 let s_gr = gr_amount * s_1gr;
@@ -12,24 +12,25 @@ console.log(s_free + 'm2');
 
 // 2_task
 let sbig_ellipse = parseInt(15 + 'dm2');
-let ssmall_ellipse = parseInt((600 / 100) + 'dm2');
+let ssmall_ellipse = parseInt( (600 / 100) + 'dm2' );
 let sring = sbig_ellipse - ssmall_ellipse;
 console.log(sring + 'dm2');
 
 // 3_task
 function popBox () 
 {
-    x = confirm(Math.min(9, 25, 13)); 
+    x = confirm( Math.min(9, 25, 13) ); 
 
     if (x == true )
     {
         document.write('Вы нажали ок');
     }
 }
-
 // 4_ task
-let res;
-let m = 6;
-let n = 11;
-res = (m >= n)? "m ближе к 10": "n ближе к 10";
+let x = 10;
+let m = Math.abs(x - '30');
+let n = Math.abs(x - '-1');
+let res = ( m < n ) ? "m ближе к 10" :
+    ( m > n ) ? "n ближе к 10" :
+    '';
 console.log(res);
